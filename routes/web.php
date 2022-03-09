e41806df7cb2677fab6168c924a38c47b8d59dd6
Pasyah Vegananda <pasyahwivegananda@gmail.com>
2022-03-09 08:32:21 +0700

    perubahan dikit

--- routes/web.php
+++ routes/web.php
@@ -106,6 +106,7 @@ Route::group(['middleware' => 'auth:web'], function() {
 
     Route::group(['middleware' => ['role:perwakilan|dekan|asesor']], function ()
     {
+        
         Route::get('/download-excel', [TataPamongController::class, 'exportToExcel']);
         Route::get('/download-csv', [TataPamongController::class, 'exportToCSV']);
         Route::get('/download-pdf', [TataPamongController::class, 'generate']);
