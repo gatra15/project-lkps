ee46ac28830be4d7cd0a356f231b9e482134ef0d
gatra15 <gatra881@gmail.com>
2022-03-10 21:16:18 +0700

    new

--- resources/views/partials/kinerjadosenmodal/penelitianedit.blade.php
+++ resources/views/partials/kinerjadosenmodal/penelitianedit.blade.php
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 <form action="/kinerja-dosen/penelitian-dtps/{{ $penelitian->id }}" method="POST">
     @method('put')
-=======
-{{-- <form action="/kinerja-dosen" method="post"> --}}
->>>>>>> 05dc11eaa73031f6da20d8c7f54eb9a6633bc0f8
     @csrf
     <div class="modal-body">
         
@@ -12,7 +8,6 @@
                
                 {{-- MASUK KE TABEL SDM DOSENS --}}
 
-<<<<<<< HEAD
                 <label for="dosentetap"> Sumber Daya Pembiyaan :</label>
                 <p>@php echo $penelitian->sumber->sumberdaya @endphp</p>
                 <div class="input-group input-group-sm mb-3">
@@ -35,52 +30,16 @@
                 <div class="input-group input-group-sm mb-3">
                     <input type="text" name="jumlah" value="{{ $penelitian->jumlah }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" required>
                 </div>
-=======
-                <label for="pengkuan"> Nama Dosen :</label>
-                <div class="input-group input-group-sm mb-3">
-                    <input type="text" name="nama" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" required>
-                </div>
-                <label for="pengkuan"> Bidang Keahlian :</label>
-                <div class="input-group input-group-sm mb-3">
-                    <input type="text" name="bidang_keahlian" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" required>
-                </div>
-                <label for="pengkuan"> Rekognisi dan Bukti Pendukung :</label>
-                <div class="input-group input-group-sm mb-3">
-                    <input type="text" name="bukti_pendukung" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" required>
-                </div>
-                <label for="pengkuan"> Tingkat :</label>
-                <div class="input-group mb-3">
-                    <select class="custom-select" name="tingkat" id="inputGroupSelect01">
-                        <option selected>pilih...</option>
-                        <option value="Wilayah">Wilayah</option>
-                        <option value="Nasional">Nasional</option>
-                        <option value="Internasional">Internsional</option>
-                    </select>
-                </div>
-                <label for="pengkuan"> Tahun:</label>
-                <div class="input-group input-group-sm mb-3">
-                    <input type="text" name="tahun" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" required>
-                </div>
-                
-                
->>>>>>> 05dc11eaa73031f6da20d8c7f54eb9a6633bc0f8
 
                {{-- coding modal insert akhir --}}
                
 
             </div>
         </div>
-<<<<<<< HEAD
     
     </div>
     <div class="modal-footer">
     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
     <button type="submit" class="btn btn-primary">Simpan</button>
-=======
-    </div>
-    <div class="modal-footer">
-    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-    <button type="submit" class="btn btn-primary">Tambah</button>
->>>>>>> 05dc11eaa73031f6da20d8c7f54eb9a6633bc0f8
     </div>
 </form>
