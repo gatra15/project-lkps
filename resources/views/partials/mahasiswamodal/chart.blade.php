64f720459503a18d1b018f67d5fe479ca45230ef
Pasyah Vegananda <pasyahwivegananda@gmail.com>
2022-03-24 18:42:42 +0700

    Update chart.blade.php

--- resources/views/partials/mahasiswamodal/chart.blade.php
+++ resources/views/partials/mahasiswamodal/chart.blade.php
@@ -3,38 +3,38 @@
 </div>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-  <script>
-    const labels = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-    ];
-  
-    const data = {
-      labels: labels,
-      datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-      }]
-    };
-  
-    const config = {
-      type: 'line',
-      data: data,
-      options: {}
-    };
-  </script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+  
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+};
+  
+const config = {
+  type: 'line',
+  data: data,
+  options: {}
+};
+</script>
+
+<script>
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
 </script>
